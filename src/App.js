54f02a05647cafbe71e21928c5a8be5667f977bf
Tarun <msgtotarun@gmail.com>
2021-02-02 15:31:33 +0530

    fixed  routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import './Route'
+import Routes from './Routes'
 function App() {
   return (
     <div className="App">
-    <route/>
+    <Routes/>
   </div>
   
   );
---
+++ src/Pages/t.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import NavBar from './../components/NavBar/NavBar';
+const test = ()=>{
+    return(
+        <div>
+    <NavBar></NavBar>
+    </div>
+        );
+}
+
+export default test
