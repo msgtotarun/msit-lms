093da5101c84b41425d716b5aab4e900cd2931b4
Tarun <msgtotarun@gmail.com>
2021-02-16 18:20:50 +0530

    merge issue resolved in login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -110,7 +110,6 @@ async handleLogin(event){
    }
   render() {
     if(localStorage.getItem('token')===null){
-<<<<<<< HEAD
 var login=
             <div className  = "d-flex justify-content-center h-100" >
             <div className  = "user_card" >
@@ -176,54 +175,5 @@ var login=
       </div>
       );
 }
-=======
-var login = (<div className="d-flex justify-content-center h-100">
-  <div className="user_card">
-    <div className="d-flex justify-content-center">
-      <div className="brand_logo_container">
-        <img src="/image.jpg" width="400" height="400" className="brand_logo" alt="" />
-      </div>
-    </div>
-    <div className="d-flex justify-content-center form_container">
-      <form>
-        < div className="input-group mb-3">
-          < div className="input-group-append">
-            < span className="input-group-text">
-              < i className="fas fa-user"> </i></span>
-    </div>
-    <input type="text" name="username" className="form-control input_user" value={this.state.username} placeholder="username" onChange={this.handleInputChange} />
-
-  </div>
-  <div className="input-group mb-2">
-    < div className="input-group-append">
-      <span className="input-group-text">
-        < i className="fas fa-key"> </i>
-      </span>
-  </div>
-  <input type="password" name="password" className="form-control input_pass" value={this.state.password} placeholder="password" onChange={this.handleInputChange} />
-
-</div>
-<div className="mt-4">
-  <div className="d-flex justify-content-center links">
-    <Link to="#"> Forgot your password ? </Link>
-  </div>
-</div>
-<div className="d-flex justify-content-center mt-3 login_container">
-  <button type="button" name="button" className="btn login_btn" onClick={this.handleLogin}> Login </button>
-</div>
-</form>
-</div>
-</div>
-</div>);
-      }
-
-      return (
-            <div className  = "container h-100 loginFix" >
-            {login}
-            </div>
-            );
-
-    }
->>>>>>> f61940ecd6e823eb19312224eecac12b10ff3cce
 }
 export default withRouter(Login);
