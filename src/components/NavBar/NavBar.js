26765aa3ace54d636377c98a5c30160f28d547e1
Tarun <msgtotarun@gmail.com>
2021-02-23 18:14:02 +0530

    no changes

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,51 +1,66 @@
 import React, { Component } from "react";
-import {Link,withRouter} from "react-router-dom";
-import './NavBar.css'
+import { Link, withRouter } from "react-router-dom";
+import "./NavBar.css";
 
 class NavBar extends Component {
-
-
   render() {
-    var isLogined=localStorage.getItem('username')!==null
-    const userName='Hi! '+localStorage.getItem('username')   
-    var home=isLogined?'/program-catalog':'/'
+    var isLogined = localStorage.getItem("username") !== null;
+    const userName = "Hi! " + localStorage.getItem("username");
+    var home = isLogined ? "/program-catalog" : "/";
     return (
       <div className='nav-fix'>
-      <nav className="navbar fixed-top navbar-expand-sm navbar-colour navbar-dark">
-      <div className ="container-fluid">
-        <Link className ="navbar-brand" to={home} >LMS</Link>
-        {/* <Link className ="navbar-brand" to='/' >LMS</Link> */}
-        <button className ="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-         aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-        </button>         
-    {isLogined && (
-      <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul className ="navbar-nav me-auto mb-lg-0">  
-                <li className ="nav-item">
-                 <Link className ="nav-link" to='#'>Credits</Link>
-                 </li>  
+        <nav className='navbar fixed-top navbar-expand-sm navbar-colour navbar-dark'>
+          <div className='container-fluid'>
+            <Link className='navbar-brand' to={home}>
+              LMS
+            </Link>
+            {/* <Link className ="navbar-brand" to='/' >LMS</Link> */}
+            <button
+              className='navbar-toggler'
+              type='button'
+              data-bs-toggle='collapse'
+              data-bs-target='#navbarNavDropdown'
+              aria-controls='navbarNavDropdown'
+              aria-expanded='false'
+              aria-label='Toggle navigation'>
+              <span class='navbar-toggler-icon'></span>
+            </button>
+            {isLogined && (
+              <div className='collapse navbar-collapse' id='navbarNavDropdown'>
+                <ul className='navbar-nav me-auto mb-lg-0'>
+                  <li className='nav-item'>
+                    <Link className='nav-link' to='#'>
+                      Credits
+                    </Link>
+                  </li>
                 </ul>
-                <ul className ="navbar-nav ms-auto">
-                   
-                    <li className ="nav-item">
-                        <Link  className ="nav-link " to="#">Course status</Link>
-                    </li>
-                    <li className ="nav-item">
-                      <span className ="nav-link active">{userName}</span>
+                <ul className='navbar-nav ms-auto'>
+                  <li className='nav-item'>
+                    <Link className='nav-link ' to='#'>
+                      Course status
+                    </Link>
                   </li>
-                  <li className ="nav-item">
-                    <Link className ="nav-link " onClick={e=>{localStorage.clear()
-                    window.location.replace('/')}}>Logout</Link>
+                  <li className='nav-item'>
+                    <span className='nav-link active'>{userName}</span>
                   </li>
-                </ul>    
-          </div>)}
+                  <li className='nav-item'>
+                    <Link
+                      className='nav-link '
+                      onClick={(e) => {
+                        localStorage.clear();
+                        window.location.replace("/");
+                      }}>
+                      Logout
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+        </nav>
       </div>
-    </nav>
-    </div>
-  )
+    );
   }
 }
-
 
 export default withRouter(NavBar);
