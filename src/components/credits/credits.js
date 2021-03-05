9bc484663771124509ae612c9b68f73da0c58585
Sravan-m <sravanm222@gmail.com>
2021-03-05 07:20:41 +0530

    Modified credits page

--- src/components/credits/credits.js
+++ src/components/credits/credits.js
@@ -1,22 +1,46 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import {Link,withRouter} from "react-router-dom";
+import NavBar from '../NavBar/NavBar';
 import './credits.css';
 
+
+
 class Credits extends Component{
+    constructor(){
+        super();
+        this.state = {
+            names : {
+                value1: "Kalluru Rohit",
+                value2: "Mallika Bhargavi",
+                value3: "Radhika Patnala",
+                value4: "Tarun Kumar",
+                value5: "Muvvala Sravan Kumar"
+            }
+        }
+
+    }
+   
     render()
     {
+    
+
         return(
-            <div className="col-sm d-flex flex-direction:column">
-                <div className ="w-25 p-3 h-25 d-inline-block">
-                    <div className="card" >
-                        <b><h3 className="card-title">{this.props.title}</h3></b>
-                        <img src={this.props.image} className="card-img-top" alt="..."/>
-                            <div className="card-body">
-                                <a href="#" className="card-link">{this.props.link1}</a>
-                                <a href="#" className="card-link">{this.props.link2}</a>
-                            </div>
-                        <div className="card-body">
-                            <h5 className="card-title">{this.props.title}</h5>
-                        </div>
+            <div>
+                <div>
+                    <h3 class="flow1">Developers</h3>
+                </div>
+                <div class="card flow">
+                    <div class="card-body">
+                        <h5 class="card-title">{this.state.names}</h5>
+                    </div>
+                    <img src={this.props.image} class="card-img-top"  />
+                    <div class="card-body">
+                        <a href={this.props.link} class="card-link">
+                            <img class="logo1" src="https://image.flaticon.com/icons/png/512/25/25231.png" />
+                        </a>
+                        <a href={this.props.link1} class="card-link">
+                            <img class="logo2" src="https://www.pngfind.com/pngs/m/57-571935_linkedin-icon-vector-png-linkedin-circle-logo-transparent.png" />
+                        </a>
                     </div>
                 </div>
             </div>
@@ -24,4 +48,4 @@ class Credits extends Component{
     }
 }
 
-export default Credits;
+export default withRouter(Credits);
